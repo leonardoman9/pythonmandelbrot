e05709a5824824dfc1cc99a88266ee6f6554efa6
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2024-02-17 19:02:42 +0100

    progress bar

--- pythonMandelbrot/plot.py
+++ pythonMandelbrot/plot.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageDraw
 from mandelbrot import mandelbrot, MAX_ITER
+from tqdm import tqdm
+from itertools import product
 
 # Image size (pixels)
 WIDTH = 1800
@@ -16,16 +18,15 @@ palette = []
 im = Image.new('RGB', (WIDTH, HEIGHT), (0, 0, 0))
 draw = ImageDraw.Draw(im)
 
-for x in range(0, WIDTH):
-    for y in range(0, HEIGHT):
-        # Convert pixel coordinate to complex number
-        c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
-                    IM_START + (y / HEIGHT) * (IM_END - IM_START))
-        # Compute the number of iterations
-        m = mandelbrot(c)
-        # The color depends on the number of iterations
-        color = 255 - int(m * 255 / MAX_ITER)
-        # Plot the point
-        draw.point([x, y], (color, color, color))
+for x, y in tqdm(product(range(WIDTH), range(HEIGHT)), total = WIDTH * HEIGHT):
+    # Convert pixel coordinate to complex number
+    c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
+                IM_START + (y / HEIGHT) * (IM_END - IM_START))
+    # Compute the number of iterations
+    m = mandelbrot(c)
+    # The color depends on the number of iterations
+    color = 255 - int(m * 255 / MAX_ITER)
+    # Plot the point
+    draw.point([x, y], (color, color, color))
 
 im.save('output.png', 'PNG')
